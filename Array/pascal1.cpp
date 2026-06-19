8a11d48c48f64f956ded0569a1052034f052375f
httpsVishu <vishusaini687@gmail.com>
2026-06-19 14:58:45 +0000

    pascal 1 added

---
+++ Array/pascal1.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> generateRows(int row) {
+        long long ans = 1;
+        vector<int> ansRow;
+        ansRow.push_back(1);
+        for (int i = 1; i < row; i++){
+            ans = ans * (row - i);
+            ans = ans / i;
+            ansRow.push_back(ans);
+        }
+        return ansRow;
+    }
+
+    vector<vector<int>> generate(int N){
+        vector<vector<int>> ans;
+        for (int i = 1; i <= N; i++){
+            ans.push_back(generateRows(i));
+        }
+        return ans;
+    }
+};
