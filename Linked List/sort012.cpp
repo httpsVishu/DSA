0eb4f798f26574a516996a1ea280c54bb2792c28
httpsVishu <vishusaini687@gmail.com>
2026-07-07 14:59:13 +0000

    new ques

---
+++ Linked List/sort012.cpp
@@ -0,0 +1,54 @@
+/* Node is defined as
+  class Node {
+  public:
+    int data;
+    Node* next;
+
+    Node(int x) {
+        data = x;
+        next = nullptr;
+    }
+};
+*/
+class Solution {
+  public:
+    Node* segregate(Node* head) {
+        if (!head || !head->next){
+            return head;
+        }
+        Node* zeroHead = new Node(-1);
+        Node* oneHead = new Node(-1);
+        Node* twoHead = new Node(-1);
+        
+        Node* zero = zeroHead;
+        Node* one = oneHead;
+        Node* two = twoHead;
+        
+        Node* temp = head;
+        while (temp != nullptr){
+            if (temp->data == 0){
+                zero->next = temp;
+                zero = zero->next;
+            }
+            else if (temp->data == 1){
+                one->next = temp;
+                one = one->next;
+            }
+            else{
+                two->next = temp;
+                two = two->next;
+            }
+            temp = temp->next;
+        }
+        zero->next = (oneHead->next) ? oneHead->next : twoHead->next;
+        one->next = twoHead->next;
+        two->next = nullptr;
+        
+        Node* newHead = zeroHead->next;
+        delete zeroHead;
+        delete oneHead;
+        delete twoHead;
+        
+        return newHead;
+    }
+};
