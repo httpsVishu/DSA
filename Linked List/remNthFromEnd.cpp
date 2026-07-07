9ff54e5d19d64a2067a30ba5c1bfde19a25a511f
httpsVishu <vishusaini687@gmail.com>
2026-07-07 15:05:12 +0000

    new ques

--- Linked List/remNthFromEnd.cpp
+++ Linked List/remNthFromEnd.cpp
@@ -8,6 +8,9 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
+ // brute force 
+ 
 class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
@@ -44,6 +47,44 @@ public:
             prev = curr;
             curr = curr->next;
         }
+        return head;
+    }
+};
+
+//using fast and slow pointer
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        if (head == nullptr) return nullptr;
+
+        ListNode* fast = head;
+        ListNode* slow = head;
+        for (int i = 0; i < n; i++){
+            fast = fast->next;
+        }
+        if (fast == nullptr){
+            ListNode* newHead = head->next;
+            delete head;
+            return newHead;
+        }
+        while (fast->next != nullptr){
+            slow = slow->next;
+            fast = fast->next;
+        }
+        ListNode* delNode = slow->next;
+        slow->next = slow->next->next;
+        delete delNode;
         return head;
     }
 };
