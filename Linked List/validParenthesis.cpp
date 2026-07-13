25bfc71160696daf7253ceb09ace2f8988a2fd01
httpsVishu <vishusaini687@gmail.com>
2026-07-13 15:02:47 +0000

    new ques

---
+++ Linked List/validParenthesis.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        int n = s.size();
+        for (int i = 0; i < n; i++){
+            if (s[i] == '(' || s[i] == '{' || s[i] == '['){
+                st.push(s[i]);
+            }
+            else{
+                if (st.empty()) return false;
+                else{
+                    char ch = st.top();
+                    if ((s[i] == '}' && ch == '{')
+                        || (s[i] == ')' && ch == '(')
+                        || (s[i] == ']' && ch == '[')){
+                            st.pop();
+                    }
+                    else return false;
+                }
+            }
+        }
+        return st.empty();
+    }
+};
