3aa421631b45c920fe733e81bbff1a83b12966f6
httpsVishu <vishusaini687@gmail.com>
2026-07-15 14:17:48 +0000

    new ques added

--- Stack and Queue/NGE.cpp
+++ Stack and Queue/NGE.cpp
@@ -15,3 +15,31 @@ class Solution {
         return ans;
     }
 };
+
+
+//leetcode 496: arr me NGE nikalo and nums ki value ke liye find kro
+
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums, vector<int>& arr) {
+        int n = arr.size();
+        stack<int> st;
+        unordered_map<int, int> mpp;
+        for (int i = n-1; i >= 0; i--){
+            while (!st.empty() && st.top() <= arr[i]){
+                st.pop();
+            } 
+            mpp[arr[i]] = st.empty() ? -1 : st.top();
+            st.push(arr[i]);
+        }
+        //now find values for nums
+        vector<int> ans;
+        for (int i = 0; i < nums.size(); i++){
+            ans.push_back(mpp[nums[i]]);
+        }
+        return ans;
+    }
+};
+
+
+//we use hasmap to find values later 
