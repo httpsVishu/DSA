365d07fab0c8f3e7fc0dcdd67e6ab66b759215be
httpsVishu <vishusaini687@gmail.com>
2026-07-29 15:33:00 +0000

    new ques

---
+++ String/tolowercase.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string toLowerCase(string s) {
+        string str = "";
+        for (int i = 0; i < s.size(); i++){
+            if (s[i] >= 'A' && s[i] <= 'Z'){
+                str += (s[i] + 32);
+            }
+            else{
+                str += s[i];
+            }
+        }
+        return str;
+    }
+};
+
+//A : 65
+//a : 97
+//diff= 32
